b3386cc1b23136328f4c297d7dd96cc219b2c12d
Jluct <panaev02@gmail.com>
2017-07-20 22:23:08 +0200

    reliz news page

--- src/Jluct/BlogBundle/Controller/DefaultController.php
+++ src/Jluct/BlogBundle/Controller/DefaultController.php
@@ -9,9 +9,20 @@ class DefaultController extends Controller
     public function indexAction($page = 1)
     {
         $em = $this->getDoctrine();
-        $posts= $em->getRepository('JluctBlogBundle:Post')->getPostsByPages($page);
+        $posts = $em->getRepository('JluctBlogBundle:Post')->getPostsByPages($page);
 
         return $this->render('JluctBlogBundle:Default:index.html.twig', ['posts' => $posts]);
+    }
+
+    public function postAction($id)
+    {
+        $em = $this->getDoctrine();
+        $post = $em->getRepository('JluctBlogBundle:Post')->findOneBy([
+            'id' => $id,
+            'public' => true
+        ]);
+
+        return $this->render('JluctBlogBundle:Default:post.html.twig', ['post' => $post]);
     }
 
 }
