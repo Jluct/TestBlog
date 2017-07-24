04c40b263b55c4800d9862db8acd80bc0658e905
Jluct <panaev02@gmail.com>
2017-07-24 21:23:58 +0200

    Added publicity switch post

--- src/Jluct/BlogBundle/Controller/Admin/PostAdminController.php
+++ src/Jluct/BlogBundle/Controller/Admin/PostAdminController.php
@@ -2,6 +2,7 @@
 
 namespace Jluct\BlogBundle\Controller\Admin;
 
+use Jluct\BlogBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
@@ -14,6 +15,30 @@ class PostAdminController extends Controller
         $posts = $em->getRepository('JluctBlogBundle:Post')->getAllPostsByPages($page, 10, 0);
 
         return $this->render('JluctBlogBundle:Admin:index.html.twig', ['posts' => $posts]);
+    }
+
+    public function publicAction($id)
+    {
+        $doctrine = $this->getDoctrine();
+        /**
+         * @var Post $post
+         */
+        $post = $doctrine->getRepository('JluctBlogBundle:Post')->findOneBy([
+            'id' => $id,
+        ]);
+
+        $post->setPublic(!$post->getPublic());
+
+        $em = $doctrine->getManager();
+        $em->persist($post);
+        $em->flush();
+
+        $this->addFlash(
+            'success',
+            'Your changes were saved!'
+        );
+
+        return $this->redirectToRoute('jluct_blog_admin_post_view');
     }
     
 }
